be13ea4d047e55c2e76440c93954c26787f853fa
Jacob Whitlow <jwhitlow45@gmail.com>
2022-11-09 23:54:39 -0800

    feat:add fitness function and agent

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import numpy as np
+import pygad.kerasga
 from kaggle_environments import make
 
 from model import create_model
-from board import isValidMove, BOARD_WIDTH, BOARD_HEIGHT
+from board import isValidMove, countEmptySpaces, BOARD_WIDTH, BOARD_HEIGHT
 
 def main():
-    create_model()
-    isValidMove(np.zeros((6,7)), 0)
+    # create_model()
+    # isValidMove(np.zeros((6,7)), 0)
+    fitness_function('test','test')
     pass
 
-CUR_MODEL = None
+CUR_MODEL = create_model()
+AGENT_PLAYER_NUMBER = 2
 
 def agent(observation, config):
+    global CUR_MODEL
+
     board = np.array(observation['board'])
     
     # get weighted results from model prediction
@@ -25,8 +30,41 @@ def agent(observation, config):
     return -1
 
 def fitness_function(solution, sol_idx):
-    env = make('connectx', debug=True)
+    global CUR_MODEL
     
+    model_weights = pygad.kerasga.model_weights_as_matrix(
+        model=CUR_MODEL,
+        weights_vector=solution
+    )
+    
+    CUR_MODEL.set_weights(weights=model_weights)
+    
+    agents = [agent, 'negamax']
+    reward_index = 0
+    if AGENT_PLAYER_NUMBER == 2:
+        agents = agents[::-1]
+        reward_index = 1
+    
+    env = make('connectx', debug=True)
+    env.reset()
+    
+    # run game with agents and get result of game
+    game_recap = env.run(agents)
+    # get reward and end state from game recap
+    reward = game_recap[-1][reward_index]['reward']
+    end_state = np.array(game_recap[-1][0]['observation']['board'])
+    
+    output = env.render(mode='html')
+    with open('game.html', 'w') as FILE:
+        FILE.write(output)
+
+    score = countEmptySpaces(end_state.reshape((BOARD_HEIGHT, BOARD_WIDTH)))
+    if reward == 1: # our agent won
+        return score
+    if reward == -1: # opponent won
+        return -score
+    return 0 # draw
+        
 
 if __name__ == '__main__':
     main()
