aa3db008cc2fc9c8629751e13d418aa120e2942d
Jacob Whitlow <jwhitlow45@gmail.com>
2022-11-10 00:22:27 -0800

    feat:change activation functions to be layers

--- model.py
+++ model.py
@@ -4,18 +4,24 @@ import keras.layers as tfl
 from board import BOARD_WIDTH, BOARD_HEIGHT
 
 def create_model():
-    leaky_relu = lambda x: max(x, 0.3)
     
     model = tf.keras.Sequential()
     model.add(tfl.Input((BOARD_HEIGHT, BOARD_WIDTH, 1)))
-    model.add(tfl.Conv2D(filters=64, kernel_size=5, padding='same'))
-    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same'))
-    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same'))
-    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same'))
+    model.add(tfl.Conv2D(filters=64, kernel_size=5, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
+    model.add(tfl.Conv2D(filters=64, kernel_size=4, padding='same', activation=None))
+    model.add(tfl.ReLU())
     model.add(tfl.Flatten())
-    model.add(tfl.Dense(64, activation=leaky_relu))
-    model.add(tfl.Dense(64, activation=leaky_relu))
-    model.add(tfl.Dense(64, activation=leaky_relu))
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
+    model.add(tfl.Dense(64, activation=None))
+    model.add(tfl.LeakyReLU())
     model.add(tfl.Dense(BOARD_WIDTH, activation='sigmoid'))
     
     model.compile(optimizer='adam')
