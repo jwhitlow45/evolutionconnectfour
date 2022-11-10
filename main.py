026b5e9f258f057923685fc34e689c9e2ed45854
Jacob Whitlow <jwhitlow45@gmail.com>
2022-11-09 23:21:27 -0800

    feat:add agent to be put into kaggle environment

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import numpy as np
+from kaggle_environments import make
+
+from model import create_model
+from board import isValidMove, BOARD_WIDTH, BOARD_HEIGHT
+
 def main():
+    create_model()
+    isValidMove(np.zeros((6,7)), 0)
     pass
+
+CUR_MODEL = None
+
+def agent(observation, config):
+    board = np.array(observation['board'])
+    
+    # get weighted results from model prediction
+    weighted_results = [(weight, col) for weight, col in zip(CUR_MODEL.predict(board), range(BOARD_WIDTH))]
+    # sort results to traverse in order of move preference
+    weighted_results.sort(key = lambda x: x[0], reverse=True)
+    
+    for _, col in weighted_results:
+        if isValidMove(board.reshape((BOARD_HEIGHT, BOARD_WIDTH)), col):
+            return col
+    return -1
+
+def fitness_function(solution, sol_idx):
+    env = make('connectx', debug=True)
+    
 
 if __name__ == '__main__':
     main()
