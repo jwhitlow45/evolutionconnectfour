c94d27d5011c9d59bc661de120eeace81ca92eea
Jacob Whitlow <jwhitlow45@gmail.com>
2022-11-09 23:23:13 -0800

    feat:add method to count empty spaces to measure fitness after game is over

--- board.py
+++ board.py
@@ -10,3 +10,11 @@ def isValidMove(state: List[List[int]], col: int):
         if state[0][col] != EMPTY:
             return False
     return True
+
+def countEmptySpaces(state:List[List[int]]):
+    empties = 0
+    for i in range(BOARD_HEIGHT):
+        for j in range(BOARD_WIDTH):
+            if state[i][j] == EMPTY:
+                empties += 1
+    return empties
